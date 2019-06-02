bce31e9ac1b20dcadde60bb0b11094f36d95d0d9
Dustin Langner <dustin.langner1@gmail.com>
2019-06-02 16:24:34 -0700

    Remove editing from authorship for now

--- client/src/views/profileViews/AuthorshipView.js
+++ client/src/views/profileViews/AuthorshipView.js
@@ -3,68 +3,31 @@ import { connect } from 'react-redux'
 import { Container, Row, Col } from 'react-bootstrap'
 import CardContainer from '../../components/CardContainer'
 
-import EditModal from '../../components/EditModal'
-import { openModal, closeModal, updateContactData } from '../../redux/actions'
-
 class AuthorshipView extends Component {
-    // Open edit modal
-    displayEditModal(title, data) {
-        this.props.dispatch(openModal(title, data))
-    }
-
-    // Close edit modal
-    closeModal() {
-        this.props.dispatch(closeModal())
-    }
-
-    // Updates user info and refreshes data
-    updateInfo(state) {
-        const metaData = this.props.metaData
-        this.props.dispatch(updateContactData(metaData, state))
-    }
-
     render() {
-        const { isFetching, isEditModalOpen, cards, modalTitle, modalData } = this.props
+        const { cards } = this.props
 
         return (
-            <div>
-                {/* Modal View displayed when editing */}
-                <EditModal
-                    show={isEditModalOpen}
-                    onClose={this.closeModal.bind(this)}
-                    onSave={this.updateInfo.bind(this)}
-                    title={modalTitle}
-                    data={modalData}
-                    loading={isFetching}
-                />
-
-                <Container>
-                    {cards.map(card => {
-                        return (
-                            <Row style={{ marginBottom: '6vh' }}>
-                                <Col xs="12">
-                                    <CardContainer
-                                        title={card.title}
-                                        data={card.data}
-                                        onEditClicked={this.displayEditModal.bind(this)}
-                                    />
-                                </Col>
-                            </Row>
-                        )
-                    })}
-                </Container>
-            </div>
+            <Container>
+                {cards.map(card => {
+                    return (
+                        <Row style={{ marginBottom: '6vh' }}>
+                            <Col xs="12">
+                                <CardContainer
+                                    title={card.title}
+                                    data={card.data}
+                                />
+                            </Col>
+                        </Row>
+                    )
+                })}
+            </Container>
         )
     }
 }
 
 function mapStateToProps(state) {
     return {
-        isFetching: state.auth.isFetching,
-        metaData: state.auth.metaData,
-        isEditModalOpen: state.auth.isEditModalOpen,
-        modalTitle: state.auth.modalTitle,
-        modalData: state.auth.modalData,
         cards: [
             {
                 title: 'GBD 2017',
