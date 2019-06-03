10f69a1f21103a8497667757199a7409cbadb4d2
Dustin Langner <dustin.langner1@gmail.com>
2019-06-03 16:34:12 -0700

    Remove shell scripts and DS Store

--- client/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo "Building npm executable"
-npm run-script build
-
-echo "Building Docker Container Image..."
-docker build --no-cache -t dlangner2019/infonexus .
-
-echo "Cleaning Up..."
-docker image prune -f
