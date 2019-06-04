597e69d2fefa0ac7a2a1ce54898a1c05868ca45c
Dustin Langner <dustin.langner1@gmail.com>
2019-06-04 14:28:03 -0700

    Add second data viz and some clean up

--- client/src/views/HomeView.js
+++ client/src/views/HomeView.js
@@ -11,13 +11,21 @@ const bannerImage = require("../images/homeBanner.png")
 
 class HomeView extends Component {
     componentDidMount() {  
-        this.initViz()  
-    }   
+        this.initViz()
+        this.initViz2()    
+    }  
+
     initViz() {  
-        const vizUrl = 'https://public.tableau.com/shared/MDSMNZPM2?:toolbar=no&:display_count=yes&:origin=viz_share_link';  
+        const vizUrl = 'https://public.tableau.com/views/IHME/MapDashboard?:embed=y&:display_count=yes&:origin=viz_share_link';  
         const vizContainer = this.vizContainer;  
         let viz = new window.tableau.Viz(vizContainer, vizUrl)  
     }
+
+    initViz2() {  
+        const vizUrl2 = 'https://public.tableau.com/views/IHME/OtherViz?:embed=y&:display_count=yes&:origin=viz_share_link';  
+        const vizContainer2 = this.vizContainer2;  
+        let viz2 = new window.tableau.Viz(vizContainer2, vizUrl2)  
+    } 
 
     render() {
         const { isAuthenticated } = this.props
@@ -48,13 +56,33 @@ class HomeView extends Component {
                     </Row>
                     <Row>
                         <Col sm="12">
-                            <h4 style={{ marginTop: '30px', marginBottom: '15px' }}>GBD Collaborator Interative Map</h4>
+                            <h4 style={{ marginTop: '30px', marginBottom: '15px' }}>
+                                GBD Collaborator Interative Map
+                            </h4>
                             <p>
-                                Explore the varies locations and expertise of GBD Collaborators below with the interactive map!
-                            </p>  
+                                Explore the varies locations and areas of expertise of GBD Collaborators below with the interactive map!
+                            </p>   
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="12">
                             <div ref={(div) => { this.vizContainer = div }} />  
                         </Col>
                     </Row>
+                    <br/>
+                    <Row>
+                        <Col sm="12">
+                            <h4 style={{ marginTop: '30px', marginBottom: '15px' }}>GBD Collaborator Interative Vizualizations</h4>
+                            <p>
+                                Interested in more analytical data about the GBD collaborator network? Gain new insight below!
+                            </p>   
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col sm="12">
+                            <div ref={(div) => { this.vizContainer2 = div }}>  </div>
+                        </Col>
+                    </Row>>
                 </Container>
             </div>
         )
